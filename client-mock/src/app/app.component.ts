0188b06f1fb09affd8dee93e6b556ee5e35d8762
Wiktor Latanowicz <wiktor@latanowicz.com>
2017-09-14 20:00:25 +0200

    client mock ng

--- client-mock/src/app/app.component.ts
+++ client-mock/src/app/app.component.ts
@@ -1,7 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit {
+  url = 'ws://localhost:8000';
+  deviceId = '12345-67890';
+  measureInterval = 100;
+  packetSize = 10;
+
+  connection: any = null;
+
+  measures: any[] = [];
+
+
+  ngOnInit(): void {
+    setTimeout(this.performMeasure.bind(this), 1000);
+  }
+
+  connect() {
+    this.connection = new WebSocket(this.url);
+  }
+
+  disconnect() {
+    this.connection.close();
+  }
+
+  performMeasure() {
+    if (this.isConnected()) {
+      this.measure();
+      this.send();
+      setTimeout(this.performMeasure.bind(this), this.measureInterval);
+    } else {
+      setTimeout(this.performMeasure.bind(this), 1000);
+    }
+  }
+
+  send() {
+    if (this.measures.length > 0 && this.measures.length >= this.packetSize) {
+      let data = null;
+      if (this.packetSize == 0) {
+        data = this.measures.shift();
+      } else {
+        data = this.measures.splice(0, this.packetSize);
+      }
+      this.connection.send(JSON.stringify(data));
+      console.log('data to send', data);
+    }
+  }
+
+  measure() {
+    this.measures.push({
+      id: this.deviceId,
+      ts: new Date().getTime(),
+      s1: Math.random() * 1000,
+      s2: Math.random() * 1000,
+    });
+  }
+
+  isConnected() {
+    return this.connection && this.connection.readyState === 1;
+  }
+}
